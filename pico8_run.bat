c0ca9e20d35a5d9ff43d9a22725f8b859152edca
cmyu <cmyu@imagis.co.kr>
2025-05-16 12:39:36 +0900

    pico run bat 에 주석추가

--- pico8_run.bat
+++ pico8_run.bat
@@ -1,5 +1,8 @@
-@echo off
+;pic8 은 실행하면 %appdata%/pico-8/pico-8/ 에서 실행됩니다.
+;개발할때 애로사항이 있으므로 아래의 명령어를 통해 
+;root_path 를 cartridge 폴더로 변경하여 pico8 을 실행합니다.
 
+@echo off
 :nohide
 cd /d %~dp0
 start "" pico-8\pico8.exe -root_path "cartridge" -windowed 1 -window_size 4
