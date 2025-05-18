67775ce9eb19617acebcdbb85f7f00ff05500e7a
Yunzzin <yjy7643878@gmail.com>
2025-05-19 01:02:44 +0900

    좌우 이동 추가

--- cartridge/webug/main.lua
+++ cartridge/webug/main.lua
@@ -1,6 +1,8 @@
 function _init()
     -- Initialize the game
     cls(0)
+    x = 64
+    y = 64
     print("Game initialized") --print(text,x,y,color : PICO-8의 팔레트 색상))
 end
 
@@ -8,17 +10,17 @@ msg = 0
 function _update()
     -- Update game logic
     print("_update")
-    if btn(0) then
-        msg=1
-       end
+    if btn(0) then x = x-2 end
+    if btn(1) then x = x+2 end
 end
+
 function _draw()
     -- Draw the screen
     cls(0)
-    print("Hello, draw!", 10, 10, 7)
 
     if msg==1 then
-    print("0번(왼쪽) 눌렀당!")
+        
     end
+    spr(1, x, y)
      
 end
