45b0e7bbd13be0f2ded42e9e3bf405bc071ac4d8
Yunzzin <yjy7643878@gmail.com>
2025-05-25 01:39:19 +0900

    양옆, 위쪽 범위 안 넘어가게 설정 스프라이트가 움직일 때 모션 추가

--- carts/webug/main.lua
+++ carts/webug/main.lua
@@ -3,37 +3,45 @@ dungbox = new_dungbox()
 function _init()
     -- Initialize the game
     cls(0)
-    
-
     x = 64
     y = 100
     g = 0.8
     print("Game initialized") --print(text,x,y,color : PICO-8의 팔레트 색상))
 end
 
-msg = 0
+msg = 1
 function _update()    
     -- Update game logic
     dungbox:update()
 
     print("_update")
-    if btn(0) then x = x-2 end
-    if btn(1) then x = x+2 end
-    if btn(4) then y = y-4 end
-    y += 0.8     
+    if btn(0) then x = x-3 msg = 1 end
+    if btn(1) then x = x+3 msg = 2 end
+    if btn(4) then y = y-5 end
+    y += 0.9     
     if(y > 100) then
         y = 100
     end
+
 end
 
 function _draw()
     -- Draw the screen
     cls(0)
     dungbox:draw()
-
-    if msg==1 then
-        
+    --print(x);
+    for i=x,x+3 do print(i) end;
+    --벽 계산
+    if(0 >= x) then x=0 end
+    if(x >= 118) then x=118 end
+    if(0 >= y) then y=0 end
+    -- 좌우 움직임
+    for i=x,x+3 do print(i) 
+        if(x % 2 == 0) then msg=2 end
+        if(x % 2 == 1) then msg=1 end
     end
-    spr(1, x, y)
+ 
+        
+    spr(msg, x, y);
      
 end
