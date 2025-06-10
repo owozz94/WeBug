cbc456cfe1c2efb837d8484adb2f114a8fdba16b
pingoflimbs <pingoflimbs@gmail.com>
2025-06-10 13:44:00 +0900

    로비화면 추가, main 수정했으니 충돌조심할것. 기존코드는 if(gamestate=GAMESTATE.STAGE1) 에 넣어놨음

--- carts/webug/lobby.lua
+++ carts/webug/lobby.lua
@@ -1,5 +1,7 @@
 function new_lobby()
+
     local lobby = {}
+    lobby.state = GAMESTATE.LOBBY -- Initial state of the lobby
 
     function lobby:update()
         if btn(BTN.LEFT) then
@@ -11,18 +13,80 @@ function new_lobby()
         if btn(BTN.LEFT) then
         end
         if btn(BTN.JUMP) then
+            return GAMESTATE.STAGE1
         end
         if btn(BTN.FIRE) then
-            return "lobby"
+            return GAMESTATE.STAGE1
         end
+        return GAMESTATE.LOBBY
     end    
 
-    function lobby:draw()
-        cls(0)
-        print("Welcome to the Lobby!", 32, 64, 7) -- Print welcome message
-        print("Press [FIRE] to start", 32, 72, 7) -- Instructions for starting
-        -- Draw any additional lobby elements here
+    function lobby:getGameState()
+        return self.state
     end
+    
+    function lobby:draw()
+        cls(2)
+
+        local start_x = 6
+        local top_y = 29
+        local char_w = 20
+        local char_h = 30
+        local gap = 4          -- 문자간격
+
+        local x = start_x
+        color(COLOR.ORANGE)
+        -- W
+        line(x, top_y, x + 3, top_y + char_h)
+        line(x + 3, top_y + char_h, x + 10, top_y + char_h / 2)
+        line(x + 10, top_y + char_h / 2, x + 17, top_y + char_h)
+        line(x + 17, top_y + char_h, x + 20, top_y)
+
+        x = x + char_w + gap
+
+        -- E
+        line(x, top_y, x, top_y + char_h)
+        line(x, top_y, x + char_w, top_y)
+        line(x, top_y + char_h / 2, x + char_w * 0.7, top_y + char_h / 2)
+        line(x, top_y + char_h, x + char_w, top_y + char_h)
+
+        x = x + char_w + gap
+
+        -- B
+        line(x, top_y, x, top_y + char_h)
+        line(x, top_y, x + char_w * 0.7, top_y)
+        line(x + char_w * 0.7, top_y, x + char_w, top_y + char_h * 0.25)
+        line(x + char_w, top_y + char_h * 0.25, x + char_w * 0.7, top_y + char_h * 0.5)
+        line(x, top_y + char_h * 0.5, x + char_w * 0.7, top_y + char_h * 0.5)
+        line(x + char_w * 0.7, top_y + char_h * 0.5, x + char_w, top_y + char_h * 0.75)
+        line(x + char_w, top_y + char_h * 0.75, x + char_w * 0.7, top_y + char_h)
+        line(x + char_w * 0.7, top_y + char_h, x, top_y + char_h)
+
+        x = x + char_w + gap
+
+        -- U
+        line(x, top_y, x, top_y + char_h * 0.9)
+        line(x + char_w, top_y, x + char_w, top_y + char_h * 0.9)
+        line(x, top_y + char_h, x + char_w, top_y + char_h)
+
+        x = x + char_w + gap
+
+        -- G
+        line(x + 5, top_y, x + char_w, top_y)
+        line(x, top_y + 5, x, top_y + char_h - 5)
+        line(x + 5, top_y + char_h, x + char_w, top_y + char_h)
+        line(x + char_w, top_y + char_h * 0.6, x + char_w, top_y + char_h)
+        line(x + char_w * 0.6, top_y + char_h * 0.6, x + char_w, top_y + char_h * 0.6)
+
+        --cuties
+        spr(0x00, 32, 127-106);
+        spr(0x10, 108, 127-76);
+        spr(0x06, 64, 127-50)
+
+        print("Press [JUMP] to start", 24, 127-10, COLOR.LIGHT_GRAY)
+    end
+    return lobby
+    
 end
 
 
--- carts/webug/main.lua
+++ carts/webug/main.lua
@@ -1,47 +1,56 @@
 dungbox = new_dungbox()
+lobby = new_lobby()
+gamestate = GAMESTATE.LOBBY
 
 function _init()
     -- Initialize the game
-    cls(0)
     x = 64
     y = 110
     g = 0.8
-    print("Game initialized") --print(text,x,y,color : PICO-8의 팔레트 색상))
+    cls(0)    
 end
 
 msg = 1
 function _update()    
-    -- Update game logic
-    dungbox:update()
 
-    print("_update")
-    if btn(0) then x = x-3 msg = 1 end
-    if btn(1) then x = x+3 msg = 2 end
-    if btn(4) then y = y-5 sfx(1) end    
-    y += 0.9     
-    if(y > 100) then
-        y = 100
+    if (gamestate == GAMESTATE.LOBBY) then
+        --LOBBY-----------------------------------------------------------
+        gamestate = lobby:update()
+    elseif (gamestate == GAMESTATE.STAGE1) then        
+        --STAGE1------------------------------------------------------------
+        dungbox:update()
+        print("_update")
+        if btn(0) then x = x-3 msg = 1 end
+        if btn(1) then x = x+3 msg = 2 end
+        if btn(4) then y = y-5 sfx(1) end    
+        y += 0.9     
+        if(y > 100) then
+            y = 100
+        end
     end
-
 end
 
 function _draw()
-    -- Draw the screen
     cls(0)
-    map(0,0,0,0,128,32);
-    --print(x);
-    for i=x,x+3 do print(i) end;
-    --벽 계산
-    if(0 >= x) then x=0 end
-    if(x >= 118) then x=118 end
-    if(0 >= y) then y=0 end
-    -- 좌우 움직임
-    for i=x,x+3 do print(i) 
-        if(x % 2 == 0) then msg=2 end
-        if(x % 2 == 1) then msg=1 end
+    if (gamestate == GAMESTATE.LOBBY) then
+        --LOBBY-----------------------------------------------------------        
+        lobby:draw()
+    elseif (gamestate == GAMESTATE.STAGE1) then
+        --STAGE1------------------------------------------------------------                
+        
+        dungbox:draw()
+        map(0,0,0,0,128,32);
+        --print(x);
+        for i=x,x+3 do print(i) end;
+        --벽 계산
+        if(0 >= x) then x=0 end
+        if(x >= 118) then x=118 end
+        if(0 >= y) then y=0 end
+        -- 좌우 움직임
+        for i=x,x+3 do print(i) 
+            if(x % 2 == 0) then msg=2 end
+            if(x % 2 == 1) then msg=1 end
+        end        
+        spr(msg+16, x, y);
     end
- 
-    dungbox:draw()
-    spr(msg+16, x, y);
-     
 end
